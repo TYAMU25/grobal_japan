f690d8145bfb5133a350e11d5dd21d640a711d18
TYAMU25 <bazirisuku.1995@gmail.com>
2021-08-29 11:53:25 +0000

    レイアウト

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,17 +1,18 @@
 class RelationshipsController < ApplicationController
   def create
     current_general_user.follow(params[:general_user_id])
-    redirect_to request.referer
+    @user = GeneralUser.find(params[:general_user_id])
+    #redirect_to request.referer
   end
 
   def destroy
     current_general_user.unfollow(params[:general_user_id])
-    redirect_to request.referer
+    @user = GeneralUser.find(params[:general_user_id])
+    #redirect_to request.referer
   end
 
   def followings
     current_general_user = GeneralUser.find(params[:general_user_id])
-    byebug
     @general_users = user.followings
   end
 
