902eb41d4b4f4a1a49216ee0416710ccee37db9a
TYAMU25 <bazirisuku.4649@icloud.com>
2021-09-30 11:53:25 +0000

    サーチ機能ページネーション

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
   def index
-    @general_users = @q.result(distinct: true)
+    @general_users = @q.result(distinct: true).page(params[:page]).per(10)
+    #@submissions = Submission.all.page(params[:page]).per(10)
+    #@submissions = current_general_user
   end
 end
